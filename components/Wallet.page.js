02a953c784965eada1ccefd6c4e92ef9943ceba5
opuopu <nurmdopu428@gmail.com>
2024-09-20 22:55:25 +0600

    fix

--- components/Wallet.page.js
+++ components/Wallet.page.js
@@ -17,11 +17,12 @@ import reactQueryConfig from '../config/reactQueryConfig';
 import { hasData } from '../helpers/helpers';
 import { setFlashMessage } from '../helpers/helpers';
 import Alert from '../components/Alert';
-import Shape from '../components/Shape'
+import Shape from '../components/Shape';
 import Image from 'next/image';
 
 function WalletPage() {
-  const capitalized = string => string.charAt(0).toUpperCase() + string.slice(1);
+  const capitalized = (string) =>
+    string.charAt(0).toUpperCase() + string.slice(1);
 
   const { authUser } = useContext(globalContext);
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -74,7 +75,7 @@ function WalletPage() {
     },
   });
 
-  console.log(data?.[0]?.data?.data?.data)
+  console.log(data?.[0]?.data?.data?.data);
   // Updating User Data On Every Time user visit wallet page
   useUpdateUserInfo();
 
@@ -93,7 +94,7 @@ function WalletPage() {
                 <Button
                   className="w-full block"
                   StartIcon={<BiTransfer size={20} color="#fffff" />}
-                  >
+                >
                   Topup Account Balance
                 </Button>
               </a>
@@ -109,85 +110,103 @@ function WalletPage() {
               <Fragment key={index}>
                 {orderBlocks.data.data.data.map((order, i) => (
                   <div
-                  key={i}
-                  className="flex justify-between items-stretch flex-wrap border-b border-gray-200 py-3"
-                >
-                  <div className="flex items-start gap-3.5">
-                    <Shape
-                      size={65}
-                      className="bg-white !ring-1 !ring-gray-200"
-                    >
-                      <Image
-                        src={`https://pay.rrrtopup.com/assets/template/images/${order?.payment_method}.png`}
-                        alt="Method"
-                        width={200}
-                        height={200}
-                        className="w-full h-auto"
-                      />
-                    </Shape>
-                    <div>
-                      {/* <h6 className='_h6'>Pending</h6> */}
-                      {/* <Badge
+                    key={i}
+                    className="flex justify-between items-stretch flex-wrap border-b border-gray-200 py-3"
+                  >
+                    <div className="flex items-start gap-3.5">
+                      <Shape
+                        size={65}
+                        className="bg-white !ring-1 !ring-gray-200"
+                      >
+                        <Image
+                          src={`https://pay.rrrtopup.com/assets/template/images/${order?.payment_method}.png`}
+                          alt="Method"
+                          width={200}
+                          height={200}
+                          className="w-full h-auto"
+                        />
+                      </Shape>
+                      <div>
+                        {/* <h6 className='_h6'>Pending</h6> */}
+                        {/* <Badge
                         type={order?.status}
                         text={order?.status === "cancel" ? "Canceled" : order?.status}
                         className="!p-0  !font-semibold !text-base"
                       /> */}
-                      <p className='text-green-500 font-semibold'> {order?.status === "cancel" ? "Canceled" : capitalized(order?.status)}
-                      </p>
-                    <div className='flex items-center gap-x-1'>
-                    <p className="_subtitle1 !text-sm">Num:</p>
-                    <p>{order?.number}</p>
-                    </div>
-                      <div className='flex items-center gap-x-1'><p className="_subtitle1 !text-sm text-gray-800">
-                        TnxID:
-                      </p>
-                      <p className='text-sm'> {order?.transaction_id || "------"}</p></div>
-                    </div>
-                    {order?.is_automated == 1 && order?.status === "pending" && (
-                      <>
-                        <Button
-                          onClick={() => {
-                            repay(order?.id);
-                          }}
-                          loading={isSubmitting}
+                        <p
+                          className={`font-semibold ${
+                            order?.status === 'cancel'
+                              ? 'text-red-500'
+                              : order?.status === 'complete'
+                              ? 'text-green-500'
+                              : order?.status === 'pending'
+                              ? 'text-black'
+                              : 'text-gray-500' // fallback color for other statuses
+                          }`}
                         >
-                          Pay
-                        </Button>
-                        <Button
-                          onClick={() => {
-                            cancelWallet(order?.id);
-                          }}
-                          loading={isSubmitting}
-                          className="bg-red-500 hover:bg-red-700"
-                        >
-                          Cancel
-                        </Button>
-                      </>
+                          {order?.status === 'cancel'
+                            ? 'Canceled'
+                            : capitalized(order?.status)}
+                        </p>
+                        <div className="flex items-center gap-x-1">
+                          <p className="_subtitle1 !text-sm">Num:</p>
+                          <p>{order?.number}</p>
+                        </div>
+                        <div className="flex items-center gap-x-1">
+                          <p className="_subtitle1 !text-sm text-gray-800">
+                            TnxID:
+                          </p>
+                          <p className="text-sm">
+                            {' '}
+                            {order?.transaction_id || '------'}
+                          </p>
+                        </div>
+                      </div>
+                      {order?.is_automated == 1 &&
+                        order?.status === 'pending' && (
+                          <>
+                            <Button
+                              onClick={() => {
+                                repay(order?.id);
+                              }}
+                              loading={isSubmitting}
+                            >
+                              Pay
+                            </Button>
+                            <Button
+                              onClick={() => {
+                                cancelWallet(order?.id);
+                              }}
+                              loading={isSubmitting}
+                              className="bg-red-500 hover:bg-red-700"
+                            >
+                              Cancel
+                            </Button>
+                          </>
+                        )}
+                    </div>
+                    <div className="flex flex-col justify-between items-end">
+                      <p className="_subtitle1 !text-sm text-gray-800">
+                        {order?.created_at}
+                      </p>
+                      <p className="_subtitle1 !text-base font-bold text-green-500">
+                        <p>{(order?.status, `BDT ${order?.amount}`)}</p>
+                      </p>
+                    </div>
+                    {order?.status === 'under_review' && (
+                      <Alert
+                        type="error"
+                        title={
+                          <span>
+                            <strong>Note:</strong> একটি সমস্যা হয়েছে! দয়া করে
+                            আমাদের Support এ যোগাযোগ করুন
+                            {/* <a href="tel:01767375523">01767375523</a> */}
+                          </span>
+                        }
+                        className="block w-full !mt-2.5"
+                      />
                     )}
                   </div>
-                  <div className="flex flex-col justify-between items-end">
-                    <p className="_subtitle1 !text-sm text-gray-800">
-                      {order?.created_at}
-                    </p>
-                    <p className="_subtitle1 !text-base font-bold text-green-500">
-                      <p>{order?.status, `BDT ${order?.amount}` }</p>
-                      
-                    </p>
-                  </div>
-                  {order?.status === "under_review" && (
-                    <Alert
-                      type="error"
-                      title={
-                        <span>
-                          <strong>Note:</strong> একটি সমস্যা হয়েছে! দয়া করে
-                          আমাদের Support এ যোগাযোগ করুন
-                          {/* <a href="tel:01767375523">01767375523</a> */}
-                        </span>
-                      }
-                      className="block w-full !mt-2.5"
-                    />
-                  )}
-                </div>
                 ))}
               </Fragment>
             ))}
